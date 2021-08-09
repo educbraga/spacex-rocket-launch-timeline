4639ebb3f13c225fca04acb41f0da3c61f35c26e
Eduardo Braga <eduardocbraga@hotmail.com>
2021-08-09 16:53:28 -0300

    Feat: Adiciona timeline

---
+++ src/components/LaunchTimelineItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-timeline-item large>
+    <template v-slot:icon>
+      <v-avatar>
+        <img :src="launch.links.mission_patch_small" />
+      </v-avatar>
+    </template>
+    <template v-slot:opposite>
+      <span>{{ launchDate }}</span>
+    </template>
+    <v-card class="elevation-2">
+      <v-card-title class="text-h5">
+        MISSION: {{ launch.mission_name }}
+      </v-card-title>
+      <v-card-subtitle>
+        ROCKET: {{ launch.rocket.rocket_name }}
+      </v-card-subtitle>
+      <v-card-text>{{ launch.details }}</v-card-text>
+    </v-card>
+  </v-timeline-item>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: ["launch"],
+  computed: {
+    launchDate() {
+      return moment(this.launch.launch_date_local).format("DD/MM/YYYY");
+    },
+  },
+};
+</script>
